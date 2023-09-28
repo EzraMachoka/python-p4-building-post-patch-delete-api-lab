9b6b55837ca7acaf052c7711db27acfa8a37333a
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-28 13:26:17 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -15,56 +15,56 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 @app.route('/')
-def home():
-    return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
+def index():
+    return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
 def bakeries():
-
-    bakeries = Bakery.query.all()
-    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
-
+    all = Bakery.query.all()
+    bakery_dict = all.to_dict()
     response = make_response(
-        bakeries_serialized,
+        jsonify(bakery_dict),
         200
     )
+    response.headers['Content-Type'] = 'application/json'
     return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-
+    specific = Bakery.query.filter_by(id=id).first()
+    bakery_dict = specific.to_dict()
     response = make_response(
-        bakery_serialized,
+        jsonify(bakery_dict),
         200
     )
+    response.headers['Content-Type'] = 'application/json'
+
     return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
-    baked_goods_by_price_serialized = [
-        bg.to_dict() for bg in baked_goods_by_price
-    ]
-    
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [bg.to_dict() for bg in baked_goods]
     response = make_response(
-        baked_goods_by_price_serialized,
+        jsonify(baked_goods_list),
         200
     )
+    response.headers['Content-Type'] = 'application/json'
     return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
-    most_expensive_serialized = most_expensive.to_dict()
-
-    response = make_response(
-        most_expensive_serialized,
-        200
-    )
-    return response
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive is not None:
+        most_expensive_dict = most_expensive.to_dict()
+        response = make_response(
+            jsonify(most_expensive_dict),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        return make_response(jsonify({'message': 'No baked goods found'}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -14,14 +14,15 @@ class Bakery(db.Model, SerializerMixin):
     serialize_rules = ('-baked_goods.bakery',)
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    baked_goods = db.relationship('BakedGood', backref='bakery')
+    bakedgoods = db.relationship("BakedGood", backref="bakery")
 
     def __repr__(self):
-        return f'<Bakery {self.name}>'
+        return f'<BakeryName: {self.name}'
+
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
@@ -29,7 +30,7 @@ class BakedGood(db.Model, SerializerMixin):
     serialize_rules = ('-bakery.baked_goods',)
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String)
     price = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
@@ -37,4 +38,5 @@ class BakedGood(db.Model, SerializerMixin):
     bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
 
     def __repr__(self):
-        return f'<Baked Good {self.name}, ${self.price}>'
+        return f'GoodsName: {self.name}'
+    
